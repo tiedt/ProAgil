59c082c17578acf30dbe1e00c4c751ed762edf31
tiedt <ree_murilo@hotmail.com>
2020-07-27 22:30:37 -0300

    correções swagger

--- ProAgil.WebAPI/Startup.cs
+++ ProAgil.WebAPI/Startup.cs
@@ -27,6 +27,7 @@ using Microsoft.OpenApi.Models;
 using ProAgil.Domain.Identity;
 using ProAgil.Repository;
 using ProAgil.Respository;
+using Swashbuckle.AspNetCore.Filters;
 
 namespace ProAgil.WebAPI
 {
@@ -43,6 +44,7 @@ namespace ProAgil.WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ProAgilContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             IdentityBuilder builder = services.AddIdentityCore<User>(options => {
                 options.Password.RequireDigit = false; //números especiais
                 options.Password.RequireNonAlphanumeric = false; // nada de alfanúmerico
@@ -57,39 +59,52 @@ namespace ProAgil.WebAPI
             builder.AddRoleManager<RoleManager<Role>>();
             builder.AddSignInManager<SignInManager<User>>();
 
-            services.AddScoped<IProAgilRepository, ProAgilRepository>();
+            //services.AddScoped<IProAgilRepository, ProAgilRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>{
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
-                    .GetBytes(Configuration.GetSection("AppSetting:Token").Value)),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                };
-            }); 
+                .AddJwtBearer(options => 
+                    {
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                                .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                            ValidateIssuer = false,
+                            ValidateAudience = false
+                        };
+                    }
+                );
             services.AddMvc(options => {
-                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
-                options.Filters.Add(new AuthorizeFilter(policy));
+                    var policy = new AuthorizationPolicyBuilder()
+                        .RequireAuthenticatedUser()
+                        .Build();
+                    options.Filters.Add(new AuthorizeFilter(policy));
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .Services.AddControllers()
+                .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling =
+                 Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
-            }
-            ).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddControllers().AddNewtonsoftJson(options => {
-                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-            });
+            services.AddScoped<IProAgilRepository, ProAgilRepository>();
+            services.AddAutoMapper(typeof(Startup));            
             services.AddCors();
-            services.AddAutoMapper(typeof(Startup));
 
             // Configurando o serviço de documentação do Swagger
             services.AddSwaggerGen(c =>
-        {
-            c.SwaggerDoc("v1", new OpenApiInfo
-            {
-                Version = "v1",
-                Title = "WebAPI",
-                Description = "WEBAPI",
-            });
+         {
+              c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+              { 
+               Description = Configuration["Autenticacao:Description"],
+               Name = Configuration["Autenticacao:Name"],
+               In = ParameterLocation.Header,
+               Type = SecuritySchemeType.ApiKey
+              });
+              c.OperationFilter<SecurityRequirementsOperationFilter>(); 
+              c.SwaggerDoc("v1", new OpenApiInfo
+              {
+               Title = Configuration["App:Title"],
+               Version = Configuration["App:Version"],
+               Description = Configuration["App:Description"]
+              });
             var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
             var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
             c.IncludeXmlComments(xmlPath);
@@ -101,28 +116,27 @@ namespace ProAgil.WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-            app.UseStaticFiles();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            } else {
+                //app.UseHsts;
             }
-
+            app.UseAuthentication();
             app.UseHttpsRedirection();
-
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseRouting();
             app.UseStaticFiles();
             app.UseStaticFiles(new StaticFileOptions(){
                 FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),@"Resources")),
                 RequestPath = new PathString("/Resources")
             });
-            app.UseAuthorization();
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Api V1");
             });
-
+           // app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
