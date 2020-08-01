b1e63ce2ebe2ff2ef886ac4035af073a0d6a094d
tiedt <ree_murilo@hotmail.com>
2020-07-31 21:56:50 -0300

    metade da aula

--- ProAgil-App/src/app/eventos/evento-edit/evento-edit.component.ts
+++ ProAgil-App/src/app/eventos/evento-edit/evento-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventoService } from 'src/app/_services/evento.service';
 import { BsModalService } from 'ngx-bootstrap/modal';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BsLocaleService } from 'ngx-bootstrap/datepicker';
 import { ToastrService } from 'ngx-toastr';
 
@@ -14,6 +14,8 @@ export class EventoEditComponent implements OnInit {
 
   titulo = 'Editar Evento';
   registerForm: FormGroup;
+  imagemURL = 'assets/upload.png';
+  evento = {};
   constructor(private eventoService: EventoService,
     private modalService: BsModalService,
     private fb: FormBuilder,
@@ -23,6 +25,40 @@ export class EventoEditComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.validation();
   }
+  onFileChange(file: FileList) {
+    const reader = new FileReader();
 
+    reader.onload = (event: any) => this.imagemURL = event.target.result;
+    reader.readAsDataURL(file[0]);
+  }
+  criaLote(): FormGroup {
+      return this.fb.group({
+        nome: ['', Validators.required],
+        quantidade: ['', Validators.required],
+        preco: ['', Validators.required],
+        dataInicio: ['', Validators.required],
+        dataFim: ['', Validators.required],
+      });
+  }
+  criaRedeSocial(): FormGroup {
+    return this.fb.group({
+        nome: ['', Validators.required],
+        url: ['', Validators.required],
+      })
+  }
+  validation() {
+    this.registerForm = this.fb.group({
+      tema: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+      local: ['', Validators.required],
+      dataEvento: ['', Validators.required],
+      qtdPessoas: ['', [Validators.required, Validators.max(12220)]],
+      telefone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      imagemURL: [''],
+      lotes: this.criaLote(),
+      redesSociais: this.criaRedeSocial() 
+    });
+  }
 }
